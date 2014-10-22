daed648e5649790f897a2e8b85a37d63da07726d
Dustin Weisner <C16dustin.weisner@usafa.edu>
2014-10-21 19:51:52 -0600

    B Fuctionality acquired

--- Code/main.c
+++ Code/main.c
@@ -8,8 +8,11 @@
 extern void init();
 extern void initNokia();
 extern void clearDisplay();
-extern void drawBlock(unsigned char row, unsigned char col);
+extern void drawBlock(signed char row, signed char col, signed char yVelocity, signed char xVelocity);
 
+#define		SCREEN_HEIGHT	8
+#define		SCREEN_WIDTH	11
+#define		BLOCK_SIZE		8
 #define		TRUE			1
 #define		FALSE			0
 #define		UP_BUTTON		(P2IN & BIT5)
@@ -21,53 +24,59 @@ extern void drawBlock(unsigned char row, unsigned char col);
 
 void main() {
 
-	unsigned char	x, y, button_press;
-	volatile unsigned char	change_press;
+	unsigned char	x, y;
+	signed char 	xVel, yVel;
+
 
 	// === Initialize system ================================================
 	IFG1=0; /* clear interrupt flag1 */
 	WDTCTL=WDTPW+WDTHOLD; /* stop WD */
-	button_press = FALSE;
-	change_press = FALSE;
 
 
 	init();
 	initNokia();
 	clearDisplay();
-	x=4;		y=4;
-	drawBlock(y,x);
+	x=9;		y=4;
+	xVel=1;		yVel=1;
+	drawBlock(y,x,yVel,xVel);
 
-	while(1) {
-		if (AUX_BUTTON == 0) {
-			while(AUX_BUTTON == 0);
-			if (change_press == TRUE){
-				change_press = FALSE;
-			} else change_press = TRUE;
-			drawBlock(y,x);
+	while(1){
+// Boundary Control for ball path and bounces
+		if (y<=0 && yVel<0) {
+
+			yVel = -1*yVel;
+		} else if (y>=SCREEN_HEIGHT && yVel>0){
+			yVel = -1*yVel;
+		}
+		if (x<=0 && xVel<0) {
+			xVel = -1*xVel;
+		} else if (x>=SCREEN_WIDTH && xVel>0){
+			xVel = -1*xVel;
 		}
 
-			if (UP_BUTTON == 0) {
-				while(UP_BUTTON == 0);
-				if (y>=1) y=y-1;
-				button_press = TRUE;
-			} else if (DOWN_BUTTON == 0) {
-				while(DOWN_BUTTON == 0);
-				if (y<=6) y=y+1;
-				button_press = TRUE;
-			} else if (LEFT_BUTTON == 0) {
-				while(LEFT_BUTTON == 0);
-				if (x>=1) x=x-1;
-				button_press = TRUE;
-			} else if (RIGHT_BUTTON == 0) {
-				while(RIGHT_BUTTON == 0);
-				if (x<=10) x=x+1;
-				button_press = TRUE;
-			}
+// Update Ball's Position
+		x=x+xVel;
+		y=y+yVel;
 
-			if (button_press) {
-				button_press = FALSE;
-				//clearDisplay();
-				drawBlock(y,x);
-			}
+/*
+		if (UP_BUTTON == 0) {
+			while(UP_BUTTON == 0);
+			if (y>=1) y=y-1;
+			button_press = TRUE;
+		} else if (DOWN_BUTTON == 0) {
+			while(DOWN_BUTTON == 0);
+			if (y<=6) y=y+1;
+			button_press = TRUE;
 		}
+
+*/
+
+		clearDisplay();
+		drawBlock(y,x,yVel,xVel);
+// Then I created a delay timer
+		long i;
+		for(i=0; i<60000; i++);
+
+	}
+
 }
